003eda850c970a89d70f7219b2b1892a0ca3db6e
bastipfister <bastian.pfister@hogrefe.com>
2023-05-31 09:04:42 +0200

    deleted unused store

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import mqttVueHook from 'mqtt-vue-hook'
-
-
 import App from './App.vue'
 import router from './router'
 
@@ -12,11 +9,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-// app.use(mqttVueHook, `ws://broker.mqttdashboard.com:8000/mqtt`, {
-//     clean: false,
-//     keepalive: 60,
-//     clientId: `mqtt_client_${Math.random().toString(16).substring(2, 10)}`,
-//     connectTimeout: 4000,
-// })
 
 app.mount('#app')
